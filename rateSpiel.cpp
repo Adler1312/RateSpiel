424701d65211f959c88abc9225bcecf11ddb236d
Adler1312 <Altrim.Muharemi@hotmail.com>
2023-08-28 23:34:23 +0200

    Anzahl Versuche hinzugefügt

--- rateSpiel.cpp
+++ rateSpiel.cpp
@@ -3,29 +3,49 @@
 #include <ctime>
 
 int main() {
-    srand(time(0));  
+    srand(time(0));
 
-    int secretNumber = rand() % 100 + 1; 
-    int guess;
-    int attempts = 0;
+    int secretNumber, guess, attempts = 0;
+    int maxAttempts = 10;  
+    bool playAgain = true;
 
     std::cout << "Willkommen bei 'Rate die Zahl'!" << std::endl;
 
-    do {
-        std::cout << "Gib eine Zahl zwischen 1 und 100 ein: ";
-        std::cin >> guess;
+    while (playAgain) {
+        secretNumber = rand() % 100 + 1;
+        attempts = 0;
 
-        attempts++;
+        std::cout << "Ich habe eine neue Zahl gewaehlt. Du hast " << maxAttempts << " Versuche." << std::endl;
 
-        if (guess > secretNumber) {
-            std::cout << "Zu hoch! Versuche es erneut." << std::endl;
-        } else if (guess < secretNumber) {
-            std::cout << "Zu niedrig! Versuche es erneut." << std::endl;
-        } else {
-            std::cout << "Richtig geraten! Die geheime Zahl war " << secretNumber << "." << std::endl;
-            std::cout << "Du hast " << attempts << " Versuch(e) gebraucht." << std::endl;
-        }
-    } while (guess != secretNumber);
+        do {
+            std::cout << "Gib eine Zahl zwischen 1 und 100 ein: ";
+            std::cin >> guess;
+
+            attempts++;
+
+            if (guess > secretNumber) {
+                std::cout << "Zu hoch! Versuche es erneut." << std::endl;
+            } else if (guess < secretNumber) {
+                std::cout << "Zu niedrig! Versuche es erneut." << std::endl;
+            } else {
+                std::cout << "Richtig geraten! Die geheime Zahl war " << secretNumber << "." << std::endl;
+                std::cout << "Du hast " << attempts << " Versuch(e) gebraucht." << std::endl;
+            }
+
+            if (attempts >= maxAttempts) {
+                std::cout << "Du hast keine Versuche mehr. Die geheime Zahl war " << secretNumber << "." << std::endl;
+                break;
+            }
+
+        } while (guess != secretNumber);
+
+        char playAgainResponse;
+        std::cout << "Moechtest du noch einmal spielen? (j/n): ";
+        std::cin >> playAgainResponse;
+        playAgain = (playAgainResponse == 'j' || playAgainResponse == 'J');
+    }
+
+    std::cout << "Vielen Dank fürs Spielen!" << std::endl;
 
     return 0;
 }
